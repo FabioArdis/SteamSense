77ad5f93447ca12d3c22e6d188b37960417ddb3b
Fabio Ardis <fabioardis@gmail.com>
2026-07-08 15:18:08 +0200

    add db/schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE users (
+  steam_id BIGINT PRIMARY KEY,
+  country VARCHAR(2),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE games(
+  appid INT PRIMARY KEY,
+
+  title VARCHAR(255) NOT NULL,
+  release_date DATE,
+  price INT CHECK (price >= 0),
+  total_achievements INT DEFAULT 0,
+
+  genres TEXT[] DEFAULT '{}',
+  tags TEXT[] DEFAULT '{}',
+
+  metacritic INT,
+  positive_reviews INT DEFAULT 0,
+  negative_reviews INT DEFAULT 0,
+
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(), 
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE user_game (
+  steam_id BIGINT REFERENCES users(steam_id) ON DELETE CASCADE,
+  appid INT REFERENCES games(appid) ON DELETE CASCADE,
+
+  PRIMARY KEY (steam_id, appid),
+
+  playtime_minutes INT DEFAULT 0 NOT NULL,
+  last_played DATE,
+
+  achievements_unlocked INT DEFAULT 0,
+
+  engagement_score REAL, 
+
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(), updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
